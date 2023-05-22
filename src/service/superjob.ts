961799d669dc7973a8ecb0ca1185dc2f8e5a4250
Aleksei <toporov.aleksei@gmail.com>
2023-05-22 03:29:40 +0300

    refactor: remove comments

--- src/service/superjob.ts
+++ src/service/superjob.ts
@@ -4,14 +4,6 @@ import { SECRET_KEY, authParams, endpoints } from './config';
 import { IToken, IVacancy } from './models';
 import { ICatalog, IFetchedVacancies } from './models';
 
-// export const SECRET_KEY = 'GEU4nvd3rej*jeh.eqp';
-// const APP_API_KEY =
-//   'v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948';
-
-// export const ENDPOINT_CATALOG = 'https://startup-summer-2023-proxy.onrender.com/2.0/catalogues/';
-
-// export const ENDPOINT_VACANIES = 'https://startup-summer-2023-proxy.onrender.com/2.0/vacancies/';
-
 export async function fetchCatalog() {
   try {
     const response = await fetch(`${endpoints.ENDPOINT_CATALOG}`, {
